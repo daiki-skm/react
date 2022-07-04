c75728b403c7daf789eedc9414b6935abb874e14
daiki328 <dk08sel11@i.softbank.jp>
2022-07-04 14:44:15 +0900

    add: fix

--- 01-hello/02-hello-world/src/App.tsx
+++ 01-hello/02-hello-world/src/App.tsx
@@ -4,16 +4,16 @@ import './App.css';
 
 // eslint-disable-next-line react/function-component-definition
 const App: FC = () => (
-  <div className="App">
-    <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
+  <div className="app">
+    <header className="app-header">
+      <img src={logo} className="app-logo" alt="logo" />
       <p>
         Edit <code>src/App.tsx</code> and save to reload.
         <br />
         Hello World!
       </p>
       <a
-        className="App-link"
+        className="app-link"
         href="https://reactjs.org"
         target="_blank"
         rel="noopener noreferrer"
