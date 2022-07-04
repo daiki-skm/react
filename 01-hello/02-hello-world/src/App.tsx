b2ae844124df1e23cfde8282569a1bb34505553f
daiki328 <dk08sel11@i.softbank.jp>
2022-07-04 13:59:48 +0900

    add: prettier

--- 01-hello/02-hello-world/src/App.tsx
+++ 01-hello/02-hello-world/src/App.tsx
@@ -8,11 +8,7 @@ const App: FC = () => (
     <header className="App-header">
       <img src={logo} className="App-logo" alt="logo" />
       <p>
-        Edit
-        {' '}
-        <code>src/App.tsx</code>
-        {' '}
-        and save to reload.
+        Edit <code>src/App.tsx</code> and save to reload.
         <br />
         Hello World!
       </p>
